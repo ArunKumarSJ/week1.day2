6e30e9a570e48d352dae0fa75cf7030a37762f05
ArunKumarSJ <88589077+ArunKumarSJ@users.noreply.github.com>
2021-08-13 10:47:32 +0530

    Day 2 Assignment

---
+++ MyCalculator.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class MyCalculator {
+
+	public static void main(String[] args) {
+		Calculator obj=new Calculator();
+		obj.add(5, 7, 9);
+		obj.sub(7, 4);
+		obj.Mul(7, 4);
+		obj.div(9, 4);
+		
+
+	}
+
+}
